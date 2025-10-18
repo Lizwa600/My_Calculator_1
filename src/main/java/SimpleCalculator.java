e7249428068a569def70f25d43a567937b88332a
Alizwa Nkofunga <wtc@pop-os.localdomain>
2025-10-18 03:34:59 +0200

    modified my calculator by adding calculate method to make it testable

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,11 +1,36 @@
 import java.util.Scanner;
 
 public class SimpleCalculator {
+
+    public static double calculate(double number1, double number2, char operator) {
+        switch (operator) {
+            case '+':
+                return number1 + number2;
+
+            case '-':
+                return number1 - number2;
+
+            case '*':
+                return number1 * number2;
+
+            case '/':
+                if (number2 != 0) {
+                    return number1 / number2;
+                } else {
+                    throw new IllegalArgumentException("Cannot divide by zero");
+                }
+
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to my simple calculator, feel free to explore and play with numbers!");
         System.out.println();
+
         System.out.print("Enter the 1st number of your choice: ");
         double num1 = scanner.nextDouble();
 
@@ -15,39 +40,13 @@ public class SimpleCalculator {
         System.out.println("Choose one operator you want to use : +, -, *, /");
         char operation = scanner.next().charAt(0);
 
-        double result;
-
-        switch (operation) {
-            case '+':
-                result = num1 + num2;
-                break;
-
-            case '-':
-                result = num1 - num2;
-                break;
-
-            case '*':
-                result = num1 * num2;
-                break;
-
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error, you cannot devide by 0");
-                    scanner.close();
-                    return;
-                }
-                break;
-
-            default:
-                System.out.println("Invalid operation, please choose one of the listed operators ");
-                scanner.close();
-                return;
+        try {
+            double result = calculate(num1, num2, operation);
+            System.out.println("Answer: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
         }
 
-        System.out.println("Answer: " + result);
         scanner.close();
     }
 }
-
